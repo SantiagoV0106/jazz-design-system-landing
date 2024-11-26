3ebf8935609c4837024390263b145b967907cdcd
cristianortiz19 <cristclar012@gmail.com>
2024-11-26 14:52:08 -0500

    Our Design Section

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 
-export function Button({ type, children, icon }) {
+export function Button({ type, children, icon, props }) {
     return (
-        <button className={`${type === 'primary' ? 'bg-secondary-blue hover:bg-hoverd-primary active:bg-active-primary' : type === 'secondary' ? 'bg-secondary-orange hover:bg-hoverd-secondary active:bg-active-secondary' : 'bg-black hover:bg-hoverd-social'} rounded-full py-4 px-8 text-white font-bold transition-all`}>
+        <button className={`${type === 'primary' ? 'bg-secondary-blue hover:bg-hoverd-primary active:bg-active-primary' : type === 'secondary' ? 'bg-secondary-orange hover:bg-hoverd-secondary active:bg-active-secondary' : 'bg-black hover:bg-hoverd-social'} ${props} rounded-full py-4 px-8 text-white font-bold transition-all`}>
             {
                 icon ?
                     <div className="flex gap-2 items-center justify-center">
